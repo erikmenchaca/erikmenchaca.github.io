14ac14258e079f2f6018a944ecf9b30e79c9291d
Erik Menchaca <erik@mail.rit.edu>
2023-12-08 17:28:11 -0500

    commit

---
+++ header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en-us">
+
+<head>
+    <title>My Website - Portfolio</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="assets/media/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+</head>
+
+<body>
+<header>
+    <h1>Erik Menchaca</h1>
+</header>
--- index.php
+++ index.php
@@ -1,19 +1,7 @@
-<!doctype html>
-<html lang="en-us">
+<?php
 
-<head>
-    <title>My Website - Portfolio</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="assets/media/favicon.ico">
-    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
-</head>
-
-<body>
-<header>
-    <h1>Erik Menchaca</h1>
-</header>
-
+include "header.php";
+?>
 <main>
     <div class="wrapper">
         <div class="profile">
@@ -78,6 +66,3 @@
     </div>
 </main>
 
-</body>
-
-</html>
